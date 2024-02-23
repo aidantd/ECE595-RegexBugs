6a753a450122c262fb447002346730c9a49bb11e
aidnatd <aidantdion@gmail.com>
2024-02-23 12:59:05 -0500

    cleanup

--- repoMiner.py
+++ repoMiner.py
@@ -19,27 +19,27 @@ def mineRepo():
 
     # Write the originally scraped information 
     df = pd.DataFrame(data=commitArray, columns=["Commit Hash", "Commit Date", "Commit Msg"], copy=False)
-    df.to_excel('output.xlsx', index=False)
+    df.to_excel("output.xlsx", index=False)
 
     # Extract year from the commit date
-    df['Year'] = pd.to_datetime(df['Commit Date'], utc=True).dt.year
+    df["Year"] = pd.to_datetime(df["Commit Date"], utc=True).dt.year
 
     # Count the number of items per year
-    items_per_year = df['Year'].value_counts().sort_index()
+    items_per_year = df["Year"].value_counts().sort_index()
 
     # Add the count of items per year to the original DataFrame
-    df = df.merge(items_per_year.rename("Commits (Bug Fixes?) Found Per Year"), left_on='Year', right_index=True, how='left')
+    df = df.merge(items_per_year.rename("Commits (Bug Fixes?) Found Per Year"), left_on="Year", right_index=True, how="left")
 
     # Save the modified DataFrame to Excel
-    df.to_excel('output.xlsx', index=False)
+    df.to_excel("output.xlsx", index=False)
 
     # Create a line graph (uncomment if wanted)
-    # plt.plot(items_per_year.index, items_per_year.values, marker='o')
-    # plt.xlabel('Year')
-    # plt.ylabel('Number of Items')
-    # plt.title('Number of Items Found Each Year')
+    # plt.plot(items_per_year.index, items_per_year.values, marker="o")
+    # plt.xlabel("Year")
+    # plt.ylabel("Number of Items")
+    # plt.title("Number of Items Found Each Year")
     # plt.grid(True)
-    # plt.savefig('line_graph.png')
+    # plt.savefig("line_graph.png")
 
 if __name__ == "__main__":
     mineRepo()
