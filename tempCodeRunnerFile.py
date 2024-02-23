bf4c7a0031d6cbe9b497a125d7699350cb5865fe
aidnatd <aidantdion@gmail.com>
2024-02-23 13:04:46 -0500

    Remove the code runner file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    # for commit in Repository("https://github.com/rust-lang/regex").traverse_commits():
-    #     if ("regex " in commit.msg) and (("bug" in commit.msg) or ("fix" in commit.msg)):
-    #         print("Found commit " + commit.hash + "\n")
-    #         print(commit.msg + "\n")
