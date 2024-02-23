5ec13adf8fdae594a93a3b5e5e419a75b654b5cc
aidnatd <aidantdion@gmail.com>
2024-02-23 11:46:06 -0500

    create basic excel logic

--- repoMiner.py
+++ repoMiner.py
@@ -1,16 +1,21 @@
 from pydriller import Repository
+from datetime import timedelta
+import pandas as pd
+import openpyxl 
 
 # Here begins a list of potentially interesting repos to mine
 #
 # https://github.com/rust-lang/regex (lots of bug fixes for regex related code)
 # https://github.com/google/re2 
-# 
 
 def main():
+    commitArray = []
     for commit in Repository("https://github.com/rust-lang/regex").traverse_commits():
         if ("regex " in commit.msg) and (("bug" in commit.msg) or ("fix" in commit.msg)):
-            print("Found commit " + commit.hash + "\n")
-            print(commit.msg + "\n")
+            commitArray.append([commit.hash, str(commit.committer_date), commit.msg, ])
+
+    df = pd.DataFrame(data=commitArray, columns=["Commit Hash", "Commit Date", "Commit Msg"], copy=False)
+    df.to_excel('output.xlsx')
 
 if __name__ == "__main__":
     main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # for commit in Repository("https://github.com/rust-lang/regex").traverse_commits():
+    #     if ("regex " in commit.msg) and (("bug" in commit.msg) or ("fix" in commit.msg)):
+    #         print("Found commit " + commit.hash + "\n")
+    #         print(commit.msg + "\n")
