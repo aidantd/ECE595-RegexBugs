9aa3ed4239dcff0279141b6919d3ea39eb61e14d
tpave <tpave98@gmail.com>
2024-03-04 20:28:56 -0500

    update to loop through a dictionary of engines and initial categorizaiton of results based on the commit messages

--- repoMiner.py
+++ repoMiner.py
@@ -9,37 +9,53 @@ import matplotlib.pyplot as plt
 # https://github.com/google/re2 
 
 def mineRepo():
-    commitArray = []
+    repoDict = {"rust": "https://github.com/rust-lang/regex.git", "re2":"https://github.com/google/re2.git"}
 
-    # Search through every commit in a given repository and return any commits containing
-    # the information we are searching for 
-    for commit in Repository("https://github.com/rust-lang/regex").traverse_commits():
-        if ("regex " in commit.msg) and (("bug" in commit.msg) or ("fix" in commit.msg)):
-            commitArray.append([commit.hash, str(commit.committer_date), commit.msg])
+    for key in repoDict:
+        commitArray = []
+        
+        # strings to check for in the repo
+        evoStrings = ["improve","add","support"]
+        maintainStrings = ["fix", "cleanup", "clean up", "sanity check"]
 
-    # Write the originally scraped information 
-    df = pd.DataFrame(data=commitArray, columns=["Commit Hash", "Commit Date", "Commit Msg"], copy=False)
-    df.to_excel("output.xlsx", index=False)
-
-    # Extract year from the commit date
-    df["Year"] = pd.to_datetime(df["Commit Date"], utc=True).dt.year
-
-    # Count the number of items per year
-    itemsPerYear = df["Year"].value_counts().sort_index()
-
-    # Add the count of items per year to the original DataFrame
-    df = df.merge(itemsPerYear.rename("Commits (Bug Fixes?) Found Per Year"), left_on="Year", right_index=True, how="left")
-
-    # Save the modified DataFrame to Excel
-    df.to_excel("output.xlsx", index=False)
-
-    # Create a line graph (uncomment if wanted)
-    # plt.plot(itemsPerYear.index, itemsPerYear.values, marker="o")
-    # plt.xlabel("Year")
-    # plt.ylabel("Number of Items")
-    # plt.title("Number of Items Found Each Year")
-    # plt.grid(True)
-    # plt.savefig("line_graph.png")
+        # Search through every commit in a given repository and return any commits containing
+        # the information we are searching for 
+        for commit in Repository(repoDict[key]).traverse_commits():
+            
+            # check if the message contains any key phrases in 
+            for s in evoStrings: 
+                if  s in commit.msg.lower():
+                    commitArray.append([commit.hash, str(commit.committer_date), commit.msg,'Evolution'])
+                    break
+            for s in maintainStrings:
+                if  s in commit.msg.lower():
+                    commitArray.append([commit.hash, str(commit.committer_date), commit.msg,'Maintainence'])
+                    break
+                
+                
+        # Write the originally scraped information 
+        df = pd.DataFrame(data=commitArray, columns=["Commit Hash", "Commit Date", "Commit Msg", "Categorization"], copy=False)
+        df.to_excel(key + "_output.xlsx", index=False)
+    
+        # Extract year from the commit date
+        df["Year"] = pd.to_datetime(df["Commit Date"], utc=True).dt.year
+    
+        # Count the number of items per year
+        itemsPerYear = df["Year"].value_counts().sort_index()
+    
+        # Add the count of items per year to the original DataFrame
+        df = df.merge(itemsPerYear.rename("Commits (Bug Fixes?) Found Per Year"), left_on="Year", right_index=True, how="left")
+    
+        # Save the modified DataFrame to Excel
+        df.to_excel("output.xlsx", index=False)
+    
+        # Create a line graph (uncomment if wanted)
+        # plt.plot(itemsPerYear.index, itemsPerYear.values, marker="o")
+        # plt.xlabel("Year")
+        # plt.ylabel("Number of Items")
+        # plt.title("Number of Items Found Each Year")
+        # plt.grid(True)
+        # plt.savefig("line_graph.png")
 
 if __name__ == "__main__":
     mineRepo()
