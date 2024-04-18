b14dc441c9ead1c31ef914d4497ce7eb72d89bf1
aidnatd <aidantdion@gmail.com>
2024-04-18 17:07:23 -0400

    some basic changes

--- src/removeExtraQuotesJson.py
+++ src/removeExtraQuotesJson.py
@@ -1,9 +1,7 @@
 def remove_quotes_after_message(line):
     message_index = line.find('"message": "')
     if message_index != -1:
-        # Locate the first occurrence of quote after "message": "
         quote_index = line.find('"', message_index + len('"message": "'))
-        # If a quote is found after "message": ", remove it
         if quote_index != -1:
             line = line[:quote_index] + line[quote_index + 1:]
     return line
@@ -14,7 +12,7 @@ def process_file(input_file, output_file):
             f_out.write(processed_line)
 
 if __name__ == "__main__":
-    input_file = "/Users/aidan/Documents/School/Purdue/AdvancedSoftwareEngineering/Code/ECE595-RegexBugs/uncategorizedData/re2_commits copy.jsonl"  # Replace with your input file name
-    output_file = "/Users/aidan/Documents/School/Purdue/AdvancedSoftwareEngineering/Code/ECE595-RegexBugs/uncategorizedData/re2_commits copy_sorted.jsonl"  # Replace with your output file name
+    input_file = "/Users/aidan/Documents/School/Purdue/AdvancedSoftwareEngineering/Code/perl_commits.jsonl" 
+    output_file = "/Users/aidan/Documents/School/Purdue/AdvancedSoftwareEngineering/Code/perl_commits_fixed.jsonl"
     process_file(input_file, output_file)
     print("Processing complete.")
