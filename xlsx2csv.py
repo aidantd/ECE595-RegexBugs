6ab3ac9f2724a9a6abeaddfbadc3f8bd1b9e434a
Michael Heinz <heinz12@purdue.edu>
2024-04-18 22:09:46 -0400

    Add support for all model data (except re2 and python re)

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -29,5 +29,7 @@ def convert_xlsx_to_csv(repository_name):
     df = df[["hash", "date", "message","Evolution?"]]
     df.to_csv(repository_name + "_all.csv", index=False)
 
-repository_name = "data/rust"
-convert_xlsx_to_csv(repository_name)
+repo_names = ['v8','rust','pcre2','java','ICU']
+for name in repo_names:
+    repository_name = "data/" + name
+    convert_xlsx_to_csv(repository_name)
