9458aed58b92e7ba51f3ec85c58d17ef86cd5d30
tpave <tpave98@gmail.com>
2024-05-01 19:15:34 -0400

    update to plotting methods

--- createMetrics.py
+++ createMetrics.py
@@ -31,36 +31,49 @@ def makeSingleEnginePlot(file,engine):
     plt.plot(evoItemsPerYear.index,evoItemsPerYear.values, marker="s")
     plt.plot(maintainItemsPerYear.index,maintainItemsPerYear.values, marker="^")
     plt.xlabel("Year")
-    plt.ylabel("Number of Items")
-    plt.title(engine.upper() + "- Number of Items Found Each Year")
+    plt.ylabel("Number of Commits")
+    plt.title(engine.upper() + "- Number of Commits Found Each Year")
     plt.legend(["All", "Evolution", "Maintenance"])
     plt.grid(True)
     #plt.show()
     plt.savefig(engine+"_line_graph_counts.png")
     
+    evoPct = []
+    maintainPct = []
+    for index in totalItemsPerYear.index:
+        if index not in evoItemsPerYear.index:
+            evoPct.append(0)
+        else:
+            evoPct.append(100*evoItemsPerYear[index]/totalItemsPerYear[index])
+            
+        if index not in maintainItemsPerYear.index:
+            maintainPct.append(0)
+        else:
+            maintainPct.append(100*maintainItemsPerYear[index]/totalItemsPerYear[index])
+                        
+            
+#
 
-
-    if not (totalItemsPerYear.index == evoItemsPerYear.index).all() and not (totalItemsPerYear.index == maintainItemsPerYear.index).all():
-        print('index not equal')
-    else:
-        plt.figure()
-        plt.plot(evoItemsPerYear.index,100*evoItemsPerYear.values/totalItemsPerYear.values, marker="s")
-        plt.plot(maintainItemsPerYear.index,100*maintainItemsPerYear.values/totalItemsPerYear.values, marker="^")
-        plt.xlabel("Year")
-        plt.ylabel("Percent of Items")
-        plt.title(engine.upper() + "- Percent of Items Found Each Year")
-        plt.legend(["Evolution", "Maintenance"])
-        plt.grid(True)
-        #plt.show()
-        plt.savefig(engine+"_line_graph_percent.png")    
+    plt.figure()
+    plt.plot(totalItemsPerYear.index,evoPct, marker="s")
+    plt.plot(totalItemsPerYear.index,maintainPct, marker="^")
+    plt.xlabel("Year")
+    plt.ylabel("Percent of Commits")
+    plt.title(engine.upper() + "- Percent of Commits Found Each Year")
+    plt.legend(["Evolution", "Maintenance"])
+    plt.grid(True)
+    #plt.show()
+    plt.savefig(engine+"_line_graph_percent.png")    
     
 
 def makeAllEnginePlot(file,engines):
+
+    
     # inputs:
     #   file - path to the csv with all engine data categorized, sheet names == engine names
     #   engines - list of engine names 
     
-#%% pull in data from csv 
+    #%% pull in data from csv 
     
     # create the figures 
     evoFig = plt.figure()
@@ -69,38 +82,75 @@ def makeAllEnginePlot(file,engines):
     maintainFig = plt.figure()
     maintainAxis = plt.gca()
     
+    pctEvoFig = plt.figure()
+    pctEvoAxis = plt.gca()
+    
+    pctEvoAgeFig = plt.figure()
+    pctEvoAgeAxis = plt.gca()
+    
     # loop through the different sheets and pull in the data to plot
     for engine in engines:
         data = pd.read_excel(file,sheet_name=engine)
         data["Year"] = pd.to_datetime(data["Date"], utc=True).dt.year
         
-#        totalItemsPerYear = data['Year'].value_counts().sort_index()
+        totalItemsPerYear = data['Year'].value_counts().sort_index()
         
         evoData = data[data['Evolution?']=='Y']
         evoItemsPerYear = evoData['Year'].value_counts().sort_index()
         
         maintainData = data[data['Evolution?']=='N']
         maintainItemsPerYear = maintainData['Year'].value_counts().sort_index()
-    
+        
+        evoPct = []
+        maintainPct = []
+        for index in totalItemsPerYear.index:
+            if index not in evoItemsPerYear.index:
+                evoPct.append(0)
+            else:
+                evoPct.append(100*evoItemsPerYear[index]/totalItemsPerYear[index])
+                
+            if index not in maintainItemsPerYear.index:
+                maintainPct.append(0)
+            else:
+                maintainPct.append(100*maintainItemsPerYear[index]/totalItemsPerYear[index])
+                
         # plot the engines over one another
         evoAxis.plot(evoItemsPerYear.index,evoItemsPerYear.values, marker="s")
         maintainAxis.plot(maintainItemsPerYear.index,maintainItemsPerYear.values, marker="s")
-        
+        pctEvoAxis.plot(totalItemsPerYear.index,evoPct, marker="s")
+        pctEvoAgeAxis.plot(totalItemsPerYear.index-totalItemsPerYear.index.tolist()[0],evoPct, marker="s")
         
     # set labels and save maintenance graph    
     evoAxis.set_xlabel("Year")
-    evoAxis.set_ylabel("Number of Items")
-    evoAxis.set_title("Evolution Number of Items Found Each Year")
+    evoAxis.set_ylabel("Number of Commits")
+    evoAxis.set_title("Evolution Number of Commits Found Each Year")
     evoAxis.legend(engines)
     evoAxis.grid(True)
     evoFig.savefig("evo_line_graph_counts.png")
     
     # set labels and save maintenance graph
     maintainAxis.set_xlabel("Year")
-    maintainAxis.set_ylabel("Number of Items")
-    maintainAxis.set_title("Maintenance Number of Items Found Each Year")
+    maintainAxis.set_ylabel("Number of Commits")
+    maintainAxis.set_title("Maintenance Number of Commits Found Each Year")
+    maintainAxis.legend(engines)
     maintainAxis.grid(True)
     maintainFig.savefig("maintain_line_graph_counts.png") 
+    
+    # set labels and save maintenance graph    
+    pctEvoAxis.set_xlabel("Year")
+    pctEvoAxis.set_ylabel("Percent of Commits")
+    pctEvoAxis.set_title("Evolution Percent of Commits Found Each Year")
+    pctEvoAxis.legend(engines)
+    pctEvoAxis.grid(True)
+    pctEvoFig.savefig("evo_line_graph_percent.png")
+    
+    # set labels and save maintenance graph    
+    pctEvoAgeAxis.set_xlabel("Age [Years]")
+    pctEvoAgeAxis.set_ylabel("Percent of Commits")
+    pctEvoAgeAxis.set_title("Evolution Percent of Commits vs Age of Engine")
+    pctEvoAgeAxis.legend(engines)
+    pctEvoAgeAxis.grid(True)
+    pctEvoAgeFig.savefig("evo_line_graph_percent_age.png")
 
 ########### end of function   
     
--- metricsScript.py
+++ metricsScript.py
@@ -6,12 +6,17 @@ Created on Mon Apr  1 20:50:40 2024
 """
 
 import createMetrics as metrics
-
+import matplotlib.pyplot as plt
+plt.close('all')
 # update this file to be the master output file with each sheet as the results for a single engine
-file = 'B:\Purdue\ECE 595 - Advanced Software Engineering\Project\ECE595-RegexBugs\ChangeLog_pcre2_exampleOutput.xlsx'
+file = 'B:\Purdue\ECE 595 - Advanced Software Engineering\Project\ECE595-RegexBugs\master_outputs_OPENAI.xlsx'
 
 # these should be the sheet names for all the engines in the output file
-engines = ["pcre2","rust"]
+engines = ["re2", "icu", "pcre2"]
+#["java","v8","python","pcre2","rust","re2","icu"]
+
+# lang specific = ["java","v8","python","rust",".net","perl"]
+# non lang specific = ["re2", "icu", "pcre2"]
 metrics.makeAllEnginePlot(file,engines)
 
 for engine in engines:
